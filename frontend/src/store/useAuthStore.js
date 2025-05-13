4ab7a631d13a9d213d6597b9a143fecc66073adc
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-05-13 13:42:38 +0530

    store update

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -31,6 +31,7 @@ export const useAuthStore = create((set) => ({
             set({ authUser: res.data });
 
             toast.success("Account Created Successfully");
+            return res.data;
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
